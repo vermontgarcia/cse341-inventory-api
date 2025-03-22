1ad7d70dd99f5c521aec8a9ee0c56eae97c539b7
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-22 01:43:28 -0600

    Passport local strategy email as usernameField

--- src/helpers/passport.js
+++ src/helpers/passport.js
@@ -1,7 +1,8 @@
 const passport = require('passport');
 const User = require('../models/User');
+const { Strategy } = require('passport-local');
 
-passport.use(User.createStrategy());
+passport.use(new Strategy({ usernameField: 'email' }, User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
